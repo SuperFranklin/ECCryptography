9b2a6bde9083d576d885ca781b576d9e4ab59a4a
franek <franciszek.slupski@supermemo.com>
2019-04-06 21:31:15 +0200

    first commit

---
+++ Curve.py
@@ -0,0 +1,36 @@
+from random import randint
+class Curve:
+    A = 0
+    B = 0
+    modulo = 13
+
+    def __init__(self, A, B, modulo):
+        self.A = A
+        self.B = B
+        self.modulo = modulo
+
+    def isCorrect(self):
+        delta = (4*(self.A^3)) + (27*(self.B^2))
+        if delta % self.modulo == 0:
+            return False
+        return True
+
+    def calculate(self, x):
+        return pow(x, 3, self.modulo) + (self.A * x) + self.B
+
+    def __str__(self):
+        return "y^2 = x^3 + {}x + {}B   modulo={}".format(self.A, self.B, self.modulo)
+
+
+def generate(p):
+    isCorrect = False
+    a = 0
+    b = 0
+    while(not isCorrect):
+        a = randint(1,10)
+        b = randint(1,10)
+        delta = (4*(a^3)) + (27*(b^2))
+        if (delta % p !=0):
+            isCorrect = True
+    return Curve(a,b,p)
+
---
+++ Main.py
@@ -0,0 +1,61 @@
+import random
+from random import randint
+from Curve import generate
+
+
+def miller_rabin(n, k):
+    if n == 2:
+        return True
+
+    if n % 2 == 0:
+        return False
+
+    r, s = 0, n - 1
+    while s % 2 == 0:
+        r += 1
+        s //= 2
+    for _ in range(k):
+        a = random.randrange(2, n - 1)
+        x = pow(a, s, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
+
+def generatePrimeNumber():
+    isPrime = False
+    while(not isPrime):
+        k = random.getrandbits(254)
+        p = ( 4*k )+ 3
+        isPrime = miller_rabin(p,128)
+
+    return p
+
+def testEuler(y, p, modulo):
+    fp = pow(y,((p-1)//2), modulo)
+    if(fp == 1):
+        return True
+    return False
+
+print("generating prime number...")
+prime = generatePrimeNumber()
+print("prime number: ", prime)
+print("generating eliptic curve...")
+curve = generate(prime)
+print("Curve: ", curve)
+print("generating point...")
+correctPoint = False
+while (not correctPoint):
+    x = random.randrange(1,prime)
+    f = curve.calculate(x)
+    test = testEuler(f, prime, curve.modulo)
+    if(test):
+        p = (prime +1) / 4
+        y = pow(f, ((prime+1)//4), curve.modulo)
+        print("Point: x=",x ,", y=", y)
+        correctPoint = True
